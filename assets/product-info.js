eda9031751e8a6e73966103e76e312d0cae77826
Darren <darrenoelofse11@gmail.com>
2025-04-02 00:10:17 +0200

    attempting to remove inactive variants from option picker and activate remaining

--- assets/product-info.js
+++ assets/product-info.js
@@ -112,7 +112,7 @@ if (!customElements.get('product-info')) {
                   //alert($(event.target).parent().next("fieldset").children().eq(0).text());
                   //$(event.target).parent().next("fieldset").remove();
                   
-                  }, 1500);
+                  }, 1000);
               
                 // is_powder = true;
                 // this.dataset.url = this.dataset.url + '?variant=46991917875452';
@@ -131,7 +131,7 @@ if (!customElements.get('product-info')) {
                 //alert($(event.target).parent().next("fieldset").children().eq(0).text());
                 //$(event.target).parent().next("fieldset").remove();
                 
-                }, 1500);
+                }, 1000);
               //  is_capsule = true;
               //  this.dataset.url = this.dataset.url + '?variant=47005405905148';
               //  var productUrl = this.dataset.url ;
