93881d808f762d213caf1030d3c5fd2b614c7181
Darren <darrenoelofse11@gmail.com>
2025-04-02 12:41:47 +0200

    add gif when price is loading for variant

--- assets/product-info.js
+++ assets/product-info.js
@@ -112,7 +112,7 @@ if (!customElements.get('product-info')) {
                   //alert($(event.target).parent().next("fieldset").children().eq(0).text());
                   //$(event.target).parent().next("fieldset").remove();
                   
-                  }, 2600);
+                  }, 600);
               
                 is_powder = true;
                 // this.dataset.url = this.dataset.url + '?variant=46991917875452';
@@ -131,7 +131,7 @@ if (!customElements.get('product-info')) {
                 //alert($(event.target).parent().next("fieldset").children().eq(0).text());
                 //$(event.target).parent().next("fieldset").remove();
                 
-                }, 2800);
+                }, 800);
                is_capsule = true;
               //  this.dataset.url = this.dataset.url + '?variant=47005405905148';
               //  var productUrl = this.dataset.url ;
