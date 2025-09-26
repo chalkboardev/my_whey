0f8cedffe644a9dc56ace7c7e8be31b1a0cf49e5
Darren <darrenoelofse11@gmail.com>
2025-09-26 10:26:57 +0200

    Update cart login

--- assets/cart-drawer.js
+++ assets/cart-drawer.js
@@ -39,7 +39,10 @@ class CartDrawer extends HTMLElement {
     setTimeout(() => {
       document.getElementById('cart_backdrop').classList.add('cart_blurry');
       this.classList.add('animate', 'active');
-      this.querySelector('.mw_corner_sale_cart_drawer').classList.add('active');
+      const sales_corner = document.querySelector('#mw_corner_sale_cart_drawer');
+      if(sales_corner){
+        this.querySelector('.mw_corner_sale_cart_drawer').classList.add('active');
+      }
       console.log('testing open cart event from cart-drawer.js cart link....');
       const flavour_link = document.querySelector('#click_flavour_modal_cart');
       const flavour_link_close = document.querySelector('#flavour_popup__close');
@@ -81,9 +84,10 @@ class CartDrawer extends HTMLElement {
   close() {
     this.classList.remove('active');
     this.querySelector('.mw_corner_sale_cart_drawer').classList.remove('active');
+    document.getElementById('cart_backdrop').classList.remove('cart_blurry');
     removeTrapFocus(this.activeElement);
     document.body.classList.remove('overflow-hidden');
-    document.getElementById('cart_backdrop').classList.remove('cart_blurry');
+
   }
 
   setSummaryAccessibility(cartDrawerNote) {
