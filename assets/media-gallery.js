a77317a10b7cbf4ef2c0238568df3c1fdbb88799
Darren <darrenoelofse11@gmail.com>
2026-04-21 23:10:06 +0200

    Cart total item count update...

--- assets/media-gallery.js
+++ assets/media-gallery.js
@@ -60,6 +60,7 @@ if (!customElements.get('media-gallery')) {
           // Don't scroll if the image is already in view
           if (activeMediaRect.top > -0.5) return;
           const top = activeMediaRect.top + window.scrollY;
+          // Uncomment to scroll if the image into view when changing variant (tab and mobile)
           //window.scrollTo({ top: top, behavior: 'smooth' });
           //window.scrollTo({ top: top - 100, behavior: 'smooth' });
         });
